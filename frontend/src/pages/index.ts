868a1956e92edcfb34aa38e6822e4b862b9a23a9
siddhantprateek <siddhantprateek@gmail.com>
2024-10-14 12:20:30 +0530

    exports updated

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,5 @@
 export { default as Home } from './home/home.pages';
 export { default as Dashboard } from './dashboard/dashboard';
 export { default as Settings } from './settings/settings.pages';
+export { default as Login } from './login/login.pages';
+export { default as Pricing } from './pricing/pricing.pages';
